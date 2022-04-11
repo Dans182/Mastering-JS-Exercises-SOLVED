c7adada91eacb51b0071e22da1bbc7259afdd967
Dans182 <dgaiteiro@gmail.com>
2022-04-11 17:35:38 +0000

    ejercicio 22 listo

--- exercises/022-isEitherEven/app.js
+++ exercises/022-isEitherEven/app.js
@@ -1,1 +1,9 @@
 // Write your function here
+const isEitherEven = (num1, num2) => {
+    if (num1 % 2 === 0 || num2 % 2 === 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
