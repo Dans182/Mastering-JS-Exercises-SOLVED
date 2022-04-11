42b410dcbc48dc726d793a0203cbd98013db6a66
Dans182 <dgaiteiro@gmail.com>
2022-04-11 17:34:50 +0000

    ejercicio 21

--- exercises/021-areBothOdd/app.js
+++ exercises/021-areBothOdd/app.js
@@ -1,2 +1,9 @@
 // Write your function here
-const areBothOdd
+const areBothOdd = (num1, num2) => {
+    if (num1 % 2 === 1 && num2 % 2 === 1) {
+        return true
+    }
+    else {
+        return false
+    }
+}
