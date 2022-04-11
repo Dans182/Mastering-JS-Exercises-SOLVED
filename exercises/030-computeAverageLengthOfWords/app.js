f46914db3032ea0df63bb8cd2207f1bcdedf54a0
Dans182 <dgaiteiro@gmail.com>
2022-04-11 19:58:00 +0000

    prueba

--- exercises/030-computeAverageLengthOfWords/app.js
+++ exercises/030-computeAverageLengthOfWords/app.js
@@ -2,3 +2,4 @@
 const computeAverageLengthOfWords = (palabra1, palabra2) => {
     return (palabra1.length + palabra2.length) / 2 
 }
+
--- exercises/031-addFullNameProperty/app.js
+++ exercises/031-addFullNameProperty/app.js
@@ -1,3 +1,17 @@
 function addFullNameProperty(obj) {
   // tu codigo aqui
+  return {fullName: obj.firstName + " " + obj.lastName}
 }
+/*{fullName: obj.firstName + " " + obj.lastName}*/
+
+
+// var person = {
+//   firstName: 'Jade',
+//   lastName: 'Smith'
+// };
+
+
+// var person2 = {
+//   fullName: 'Jade' + " " + 'Smith'
+// };
+
