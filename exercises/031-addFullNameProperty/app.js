dcfb2657a807280ddf573cfeb620690091420ad2
Dans182 <dgaiteiro@gmail.com>
2022-04-11 20:38:05 +0000

    31

--- exercises/031-addFullNameProperty/app.js
+++ exercises/031-addFullNameProperty/app.js
@@ -1,17 +1,6 @@
 function addFullNameProperty(obj) {
   // tu codigo aqui
-  return {fullName: obj.firstName + " " + obj.lastName}
+  obj.fullName = obj.firstName + " " + obj.lastName
+  return obj
 }
-/*{fullName: obj.firstName + " " + obj.lastName}*/
-
-
-// var person = {
-//   firstName: 'Jade',
-//   lastName: 'Smith'
-// };
-
-
-// var person2 = {
-//   fullName: 'Jade' + " " + 'Smith'
-// };
 
