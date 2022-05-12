2823c6cfa9f22529f74e676ed0fa27791c2694c5
Dans182 <dgaiteiro@gmail.com>
2022-05-12 22:55:25 +0000

    57

--- exercises/057-getAllElementsButLast/app.js
+++ exercises/057-getAllElementsButLast/app.js
@@ -1,4 +1,7 @@
 function getAllElementsButLast(array) {
     // your code here
-
+    return array.slice(0,3)
 }
+var input = [1, 2, 3, 4];
+var output = getAllElementsButLast(input);
+console.log(output); // --> [1, 2 , 3]
