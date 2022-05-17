205e31baeef19e5af8b10ed31e1882b1924b3dd4
Dans182 <dgaiteiro@gmail.com>
2022-05-17 11:14:04 +0000

    60 listo

--- exercises/060-removeFromFrontOfNew/app.js
+++ exercises/060-removeFromFrontOfNew/app.js
@@ -1,3 +1,10 @@
 function removeFromFrontOfNew(arr) {
     // your code here
+    let newArray = arr.shift()
+    return arr
 }
+
+var arr = [1, 2, 3];
+var output = removeFromFrontOfNew(arr);
+console.log(output); // --> [2, 3]
+console.log(arr); // --> [1, 2, 3]
