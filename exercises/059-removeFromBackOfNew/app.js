ca7c2206feed3e754edafaa35915a11e8e4b49b2
Dans182 <dgaiteiro@gmail.com>
2022-05-17 11:12:10 +0000

    59 listo

--- exercises/059-removeFromBackOfNew/app.js
+++ exercises/059-removeFromBackOfNew/app.js
@@ -1,3 +1,9 @@
 function removeFromBackOfNew(arr) {
   // your code here
+  arr.pop()
+  return arr
 }
+var arr = [1, 2, 3];
+var output = removeFromBackOfNew(arr);
+console.log(output); // --> [1, 2]
+console.log(arr); // --> [1, 2, 3]
